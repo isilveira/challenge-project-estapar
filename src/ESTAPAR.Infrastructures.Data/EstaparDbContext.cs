02c913b8e2498329ff7989090b3b90ff361d921a
Ítalo Silveira <italobrian@gmail.com>
2020-07-11 10:32:33 -0300

    Add EstaparDbContext

---
+++ src/ESTAPAR.Infrastructures.Data/EstaparDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ESTAPAR.Infrastructures.Data
+{
+    public class EstaparDbContext : DbContext
+    {
+        protected EstaparDbContext()
+        {
+        }
+        public EstaparDbContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
